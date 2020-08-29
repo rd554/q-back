b6e01bd32f14a44637af70aa40b917ed3b285113
Ravi <quonquer@gmail.com>
2020-08-29 12:06:54 +0530

    done

--- controllers/form.js
+++ controllers/form.js
@@ -2,14 +2,13 @@ const ejs = require("ejs");
 const transporter = require("../helpers/mailConfig");
 
 exports.sendMail = (req, res) => {
-  const { name, email, message, subject } = req.body;
+  const { name, email, message } = req.body;
 
   const _path = `${__dirname}/../public/test.ejs`;
   const userTemplate = {
     name,
     message,
     email,
-    subject,
   };
   ejs.renderFile(_path, { userTemplate }, (error, result) => {
     if (error) {
