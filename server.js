18fa3b8dc2bdadb1cd2188b775e7ba0d5a4c68af
Ravi <quonquer@gmail.com>
2020-10-14 03:04:21 +0530

    changed to atlas

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const multer = require("multer");
-const AWS = require("aws-sdk");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -59,41 +57,3 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// AWS S3
-// const s3 = new AWS.S3({
-//   accessKeyId: process.env.AWS_ID,
-//   secretAccessKey: process.env.AWS_SECRET,
-// });
-
-// // using multer
-// const storage = multer.memoryStorage({
-//   destination: function (req, file, callback) {
-//     callback(null, "");
-//   },
-// });
-
-// const upload = multer({ storage }).single("photo");
-
-// app.post("/upload", upload, (req, res) => {
-//   const file = req.file;
-//   // let myFile = req.file.originalname.split(".");
-//   // const fileType = myFile[myFile.length - 1];
-
-//   // console.log(req.file);
-//   // res.send({
-//   //   msg: "Hello World",
-//   // });
-
-//   const params = {
-//     Bucket: process.env.AWS_BUCKET_NAME,
-//     Key: file.originalname,
-//     Body: file.buffer,
-//   };
-
-//   s3.upload(params, (error, data) => {
-//     if (error) {
-//       res.status(500).send(error);
-//     }
-//     res.status(200).send(data);
-//   });
-// });
